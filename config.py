2dc731a6452bc0073c84b2b54a0e6099a914e622
newkrishna983-lang <newkrishna983@gmail.com>
2026-05-21 07:01:59 +0530

    Change sensitive values in config.py
    Updated sensitive configuration values for BOT_TOKEN, OWNER, and LOG.

--- config.py
+++ config.py
@@ -4,13 +4,13 @@ API_ID = API_ID =  24798261
 
 API_HASH = os.environ.get("API_HASH", "fef280037f5759eccc540c6d7a279a14")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7262347573:AAF6G6jCXC5gOMz-ubKoJKPbaoZDnYF7sgY")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8582068316:AAFGRnCgt80M5cvR0jXpiPBbVZPrfb8dY8c")
 
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
-OWNER = int(os.environ.get("OWNER", 6155478725))
+OWNER = int(os.environ.get("OWNER", 5349573682))
 
-LOG = -1002249382208,
+LOG = -1003621974261,
 
 # UPDATE_GRP = -1002140332321, # bot sat group
 
@@ -18,7 +18,7 @@ LOG = -1002249382208,
 
 try:
     ADMINS=[]
-    for x in (os.environ.get("ADMINS", "6155478725").split()):
+    for x in (os.environ.get("ADMINS", "5349573682").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
